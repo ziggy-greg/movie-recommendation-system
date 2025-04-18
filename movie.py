fd7271a369b1c3f87d232b6f9930a23d07e21b90
“ziggy-greg” <ziculturestore@gmail.com>
2025-04-17 18:45:04 -0700

    model error fix

--- movie.py
+++ movie.py
@@ -1,33 +1,23 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import requests
-import os
-from surprise import Dataset, Reader, SVD
-from surprise.model_selection import train_test_split
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Load dataset
-@st.cache_data
-def load_data():
-    ratings = pd.read_csv("u.data", sep='\t', names=["user_id", "movie_id", "rating", "timestamp"])
-    movies = pd.read_csv("u.item", sep='|', encoding="ISO-8859-1", usecols=[0, 1], names=["movie_id", "title"], engine='python')
-    return pd.merge(ratings, movies, on="movie_id"), movies
+ratings = pd.read_csv("/content/u.data", sep='\t', names=["user_id", "movie_id", "rating", "timestamp"])
+movies = pd.read_csv("/content/u.item", sep='|', encoding="ISO-8859-1", usecols=[0, 1], names=["movie_id", "title"], engine='python')
+df = pd.merge(ratings, movies, on="movie_id")
 
-df, movies = load_data()
+# Create pivot table for users and movies
+pivot_table = df.pivot_table(index='user_id', columns='title', values='rating').fillna(0)
 
-# Build surprise dataset
-reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df[['user_id', 'movie_id', 'rating']], reader)
-trainset, testset = train_test_split(data, test_size=0.2, random_state=42)
-
-# Train model
-model = SVD()
-model.fit(trainset)
-
-# Build movie-id to title mapping
-movie_id_to_title = dict(zip(movies.movie_id, movies.title))
+# Compute cosine similarity between users
+user_similarity = cosine_similarity(pivot_table)
+user_similarity_df = pd.DataFrame(user_similarity, index=pivot_table.index, columns=pivot_table.index)
 
 # TMDB API Setup
-api_key = os.getenv("TMDB_API_KEY", "87ca1a58d8a6f4e3bea559458135cde2")
+api_key = "87ca1a58d8af64e3bea559458135cde2"
 def fetch_movie_details(title):
     url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={title}"
     response = requests.get(url)
@@ -44,34 +34,42 @@ def fetch_movie_details(title):
 st.set_page_config(page_title="Movie Recommender", layout="centered")
 st.title("\U0001F3AC Movie Recommendation System")
 
-user_id_input = st.number_input("Select User ID to Recommend For", min_value=1, max_value=1000, step=1)
+user_id_input = st.number_input("Select User ID to Recommend For", min_value=1, max_value=int(df.user_id.max()), step=1)
 
 if st.button("Get Recommendations"):
-    user_movies = df[df['user_id'] == user_id_input]['movie_id'].unique()
-    all_movie_ids = df['movie_id'].unique()
-    unseen_movies = [movie for movie in all_movie_ids if movie not in user_movies]
+    if user_id_input not in user_similarity_df.index:
+        st.warning("User ID not found.")
+    else:
+        similar_users = user_similarity_df[user_id_input].sort_values(ascending=False).drop(user_id_input)
+        top_similar_user_id = similar_users.index[0]
 
-    predictions = [model.predict(user_id_input, movie_id) for movie_id in unseen_movies]
-    predictions.sort(key=lambda x: x.est, reverse=True)
-    top_predictions = predictions[:10]
+        user_movies = df[df['user_id'] == user_id_input]['movie_id'].unique()
+        similar_user_movies = df[df['user_id'] == top_similar_user_id]
+        unseen_recommendations = similar_user_movies[~similar_user_movies['movie_id'].isin(user_movies)]
 
-    for pred in top_predictions:
-        movie_id = pred.iid
-        title = movie_id_to_title.get(movie_id, "Unknown Title")
-        poster_url, overview = fetch_movie_details(title)
+        top_recs = unseen_recommendations.groupby('movie_id').agg({'rating': 'mean'}).sort_values(by='rating', ascending=False).head(10)
+        top_movie_ids = top_recs.index.tolist()
 
-        st.subheader(f"{title} ({round(pred.est, 1)}⭐)")
-        col1, col2 = st.columns([1, 3])
-        with col1:
-            if poster_url:
-                st.image(poster_url, width=150)
-            else:
-                st.write("No poster available")
-        with col2:
-            st.write(overview)
+        movie_id_to_title = dict(zip(movies.movie_id, movies.title))
+
+        for movie_id in top_movie_ids:
+            title = movie_id_to_title.get(movie_id, "Unknown Title")
+            avg_rating = round(top_recs.loc[movie_id, 'rating'], 1)
+            poster_url, overview = fetch_movie_details(title)
+
+            st.subheader(f"{title} ({avg_rating}⭐)")
+            col1, col2 = st.columns([1, 3])
+            with col1:
+                if poster_url:
+                    st.image(poster_url, width=150)
+                else:
+                    st.write("No poster available")
+            with col2:
+                st.write(overview)
 
 st.markdown("---")
 st.markdown("Made by Ziggy Greg | [GitHub](https://github.com/ziggy-greg)")
+
 
 
 
